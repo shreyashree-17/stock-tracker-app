103653bd000452159242f3fbc0a0befd14d768c6
Shreya Shree <shreyaashree.17@gmail.com>
2023-09-05 22:54:09 +0530

    responsive

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,4 @@
-'use client';
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
@@ -13,33 +12,58 @@ import PieChart from '../Charts/PieChart';
 import WelcomeMsg from '../WelcomeMsg/WelcomeMsg';
 
 export default function Dashboard({ handleThemeChange }) {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    
+
+    const toggleSidebar = () => {
+        if (window.innerWidth <= 768) {
+            setIsSidebarOpen(false);
+        } else {
+            setIsSidebarOpen(true);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', toggleSidebar);
+        toggleSidebar();
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', toggleSidebar);
+        };
+    }, []);
+
+    const togglesmallSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
         <Box sx={{ display: 'flex' }}>
             <CssBaseline />
-            <Navbar handleThemeChange={handleThemeChange} />
-            <Sidebar />
+            <Navbar handleThemeChange={handleThemeChange} toggleSidebar={togglesmallSidebar}/>
+            {isSidebarOpen && <Sidebar />} {/* Show the sidebar only when isSidebarOpen is true */}
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 <Toolbar />
                 <Grid container spacing={2}>
-                    <Grid item xs={7}>
+                    <Grid item xs={12} md={7}> {/* Adjust grid layout for different screen sizes */}
                         <WelcomeMsg />
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={12} md={5}> {/* Adjust grid layout for different screen sizes */}
                         <BarChart />
                     </Grid>
-                    <Grid item xs={3.5}>
+                    <Grid item xs={12} md={3.5}> {/* Adjust grid layout for different screen sizes */}
                         <UpdateChart index={0} />
                     </Grid>
-                    <Grid item xs={3.5}>
+                    <Grid item xs={12} md={3.5}> {/* Adjust grid layout for different screen sizes */}
                         <UpdateChart index={1} />
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={12} md={5}> {/* Adjust grid layout for different screen sizes */}
                         <UpdateChart index={2} />
                     </Grid>
-                    <Grid item xs={7}>
+                    <Grid item xs={12} md={7}> {/* Adjust grid layout for different screen sizes */}
                         <ExpandedBarChart />
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={12} md={5}> {/* Adjust grid layout for different screen sizes */}
                         <PieChart />
                     </Grid>
                 </Grid>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,51 @@
 import { AppBar, Avatar, Container, Divider, IconButton, Switch, Toolbar } from "@mui/material";
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ShowChartIcon from '@mui/icons-material/ShowChart';
 import styles from "./navbar.module.css"
 import ThemeSwitcher from "../ThemeSwitcher";
 import Searchbar from "./Searchbar";
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default function Navbar({ handleThemeChange }) {
-    return <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-            <Container sx={{ flexGrow: 1, marginLeft: 0, display: "flex", alignItems: "center" }}>
-                <ShowChartIcon /><b>Tracker</b>
-                <Searchbar />
-            </Container>
-            <ThemeSwitcher handleThemeChange={handleThemeChange} />
-            <IconButton sx={{ mr: 1 }}>
-                <NotificationsIcon />
-            </IconButton>
-            <Divider orientation="vertical" flexItem />
-            <p className={styles.userName}>User Name</p>
-            <Avatar sx={{ width: 30, height: 30 }} alt="User Name" src="https://images.unsplash.com/photo-1613679074971-91fc27180061?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" />
-        </Toolbar>
-    </AppBar>
+
+export default function Navbar({ handleThemeChange, toggleSidebar}) {
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+
+    const handleResize = () => {
+        setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+            <Toolbar>
+                <Container sx={{ flexGrow: 1, marginLeft: 0, display: "flex", alignItems: "center" }}>
+                    {isSmallScreen && <IconButton
+                        color="inherit"
+                        aria-label="Open Menu"
+                        edge="start"
+                        onClick={toggleSidebar} 
+                    >
+                        <MenuIcon/> {/* Add the Hamburger icon */}
+                    </IconButton>}
+                    <ShowChartIcon /><b>Tracker</b>
+                    <Searchbar />
+                </Container>
+                <ThemeSwitcher handleThemeChange={handleThemeChange} />
+                <IconButton sx={{ mr: 1 }}>
+                    <NotificationsIcon />
+                </IconButton>
+                <Divider orientation="vertical" flexItem />
+                <p className={styles.userName}>User Name</p>
+                <Avatar sx={{ width: 30, height: 30 }} alt="User Name" src="https://images.unsplash.com/photo-1613679074971-91fc27180061?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" />
+            </Toolbar>
+        </AppBar>
+    );
 }
